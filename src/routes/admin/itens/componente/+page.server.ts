9cebdd736c2bfcb8bfd7303a9a58353b7f873e82
Leobfreitas <leonardobfreitas27@gmail.com>
2024-03-07 17:31:28 -0300

    Alteração de Status

--- src/routes/admin/itens/componente/+page.server.ts
+++ src/routes/admin/itens/componente/+page.server.ts
@@ -10,7 +10,7 @@ export const load: PageServerLoad = async ({cookies})=>{
 }
 
 export const actions = {
-    default: async({request})=>{
+    create: async({request})=>{
         const data = await request.formData();
         const componente = {
             nome: data.get("nome") as string
@@ -18,5 +18,10 @@ export const actions = {
 
         await prisma.componente.create({data: componente})
 
-    }
+    },
+    delete: async({request})=>{
+        const data = await request.formData();
+        const id = parseInt(data.get("id") as string);
+        await prisma.componente.delete({where: {id}})
+    }    
 } satisfies Actions;
