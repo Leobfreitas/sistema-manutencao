0496280eb7e1c1a35ad4095f38ae7ae10ab9c326
Bruno de Castro Brezolin <bbrezolin@hotmail.com>
2024-03-06 23:31:41 -0300

    att autenticação de admin

--- src/routes/admin/+layout.server.ts
+++ src/routes/admin/+layout.server.ts
@@ -1,18 +1,11 @@
-import { decodificaToken, verificaToken } from "$lib/crypto";
-import { error, redirect } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
 import type { LayoutServerLoad } from "./$types";
 import { TipoUsuario } from "prisma/prisma-client";
+import { getUserFromToken } from "$lib";
 
-export const load: LayoutServerLoad = async ({cookies}) =>{
-
+export const load: LayoutServerLoad = async ({ cookies }) => {
     const token = cookies.get("token");
-    if(!token){
-        throw error(401, "Usuário não está logado");
-    }
-    if(await verificaToken(token)){
-        const decodificado = await decodificaToken(token);
-        if(decodificado.cargo != TipoUsuario.ADMINISTRADOR){
-            throw error(401, "Usuário não tem permissão");
-        }
-    }
+    const usuario = await getUserFromToken(token);
+    if (!usuario) throw error(401, "Usuário não autenticado");
+    if (usuario.cargo !== TipoUsuario.ADMINISTRADOR) throw error(403, "Usuário não autorizado");
 }
