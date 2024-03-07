bf769040a119a9002dbb5ab16ffcda9a70fa6338
Bruno de Castro Brezolin <bbrezolin@hotmail.com>
2024-03-06 23:29:51 -0300

    nova rotina de autenticação

--- src/lib/crypto.ts
+++ src/lib/crypto.ts
@@ -5,23 +5,19 @@ import "dotenv/config";
 
 const JWT_SECRET = process.env.JWT_SECRET ?? "batata";
 
-export async function hashSenha(senha: string){
+export async function hashSenha(senha: string) {
     const salt = await bcrypt.genSalt(10);
     return bcrypt.hash(senha, salt);
 }
 
-export async function comparaSenha(senha: string, hash: string){
+export async function comparaSenha(senha: string, hash: string) {
     return bcrypt.compare(senha, hash);
 }
 
-export async function geraToken(usuario: Usuario){
+export async function geraToken(usuario: Usuario) {
     return jwt.sign(usuario, JWT_SECRET);
 }
 
-export async function decodificaToken(token: string){
-    return jwt.decode(token) as Usuario;
-}
-
-export async function verificaToken(token: string){
-    return jwt.verify(token, JWT_SECRET);
+export async function verificaToken(token: string) {
+    return jwt.verify(token, JWT_SECRET) as Usuario;
 }
