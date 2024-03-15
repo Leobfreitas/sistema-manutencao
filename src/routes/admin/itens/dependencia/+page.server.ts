992d8b8912162fe4c6c09554772063434cf121a4
Bruno de Castro Brezolin <bbrezolin@hotmail.com>
2024-03-15 17:19:27 -0300

    delete de dependencia

--- src/routes/admin/itens/dependencia/+page.server.ts
+++ src/routes/admin/itens/dependencia/+page.server.ts
@@ -1,16 +1,16 @@
 import { prisma } from "$lib";
 import type { Actions, PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({cookies})=>{
+export const load: PageServerLoad = async ({ cookies }) => {
     const dependencia = await prisma.dependencia.findMany();
-    
+
     return {
         dependencia
     };
 }
 
 export const actions = {
-    default: async({request})=>{
+    default: async ({ request }) => {
         const data = await request.formData();
         const dependencia = {
             nome: data.get("nome") as string,
@@ -18,7 +18,13 @@ export const actions = {
             siglaDependencia: data.get("siglaDependencia") as string
         };
 
-        await prisma.dependencia.create({data: dependencia});
+        await prisma.dependencia.create({ data: dependencia });
 
+    },
+
+    delete: async ({ request }) => {
+        const data = await request.formData();
+        const id = parseInt(data.get("id") as string);
+        await prisma.dependencia.delete({ where: { id } })
     }
 } satisfies Actions;
